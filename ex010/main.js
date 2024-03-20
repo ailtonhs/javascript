9813460d30335280a94e047c0e38d4ce5524696c
Ailton <ailtonhenrick@outlook.com>
2024-03-20 13:28:02 -0300

    Usando js externo

---
+++ ex010/main.js
@@ -0,0 +1,20 @@
+let res = document.getElementsByTagName('p')[0];
+
+function acao1() {
+    res.innerHTML += `<p>Clicou no primeiro botão</p>`;
+    
+}
+
+function acao2() {
+    res.innerHTML += `<p>Clicou no segundo botão</p>`
+    
+}
+
+function acao3() {
+    res.innerHTML += `<p>Clicou no terceiro botão</p>`
+}
+
+function acao4() {
+    res.innerHTML += `<p>Clicou no quarto botão</p>`
+}
+
